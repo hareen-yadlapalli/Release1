614b07a3d91ff40d098230909fb1de5388a8c749
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-04-11 11:54:08 +1000

    SQLs added

---
+++ SQLs/Implementation/01_Impl_SQL.sql
@@ -0,0 +1,18 @@
+USE [demodb]
+GO
+
+INSERT INTO [dbo].[Persons]
+           ([PersonID]
+           ,[LastName]
+           ,[FirstName]
+           ,[Address]
+           ,[City])
+     VALUES
+           (123,
+           'def',
+           'def',
+           'def',
+           'def')
+GO
+
+
---
+++ SQLs/Rollback/01_RB_SQL.sql
@@ -0,0 +1,7 @@
+USE [demodb]
+GO
+
+delete from dbo.persons where PersonID=123
+GO
+
+
